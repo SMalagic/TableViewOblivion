6caa24bec7986cac44e87c7f6a603384bce662d2
Serkan Malagiç <serkan.malagic@portalgrup.com>
2020-10-30 13:08:16 +0300

    - initial commit

---
+++ StackViewExample/ThirdTableViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ThirdTableViewTableViewCell.swift
+//  StackViewExample
+//
+//  Created by Serkan Malagiç on 30.10.2020.
+//
+
+import UIKit
+
+class ThirdTableViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
